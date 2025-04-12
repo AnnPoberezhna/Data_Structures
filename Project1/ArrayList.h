f728310aa5470c9eb69a0c30367d509a281d1d5e
AnnPoberezhna <poberezhna.anna907@gmail.com>
2025-04-12 21:37:56 +0200

    Added implementation of functions

--- Project1/ArrayList.h
+++ Project1/ArrayList.h
@@ -1,6 +1,8 @@
 #ifndef ARRAYLIST_H
 #define ARRAYLIST_H
 
+#include <random>
+
 template <class T>
 class ArrayList{
     private:
@@ -8,23 +10,148 @@ class ArrayList{
         int size;           // current number of elements in array
         int capacity;       // total space available in array
 
-        void resize();
+        void resize() {
+            capacity *= 2;
+            
+            // Create a new array with the increased capacity
+            T* newArr = new T[capacity];
+            
+            // Copy elements from the old array to the new one
+            for (int i = 0; i < size; i++) {
+                newArr[i] = arr[i];
+            }
+            
+            // Delete old array
+            delete[] arr;
+            
+            // Updating the pointer to a new array
+            arr = newArr;
+        }
 
     public:
-        ArrayList();               // Constructor
-        ~ArrayList();             // Deconstructor
-        void addToEnd(T value);     // Add a value to the end
-        void addToStart(T value);   // Adds an element at the beginning
-        void addAtRandom(T value);  // Adds an element at random position
-        void removeLast();          // Deletes the last item
-        void removeFirst();         // Deletes the first item
-        void removeRandom();        // Deletes an item at random position
-        bool contains(T value);     // Checks if value exists in the array
-        int getSize() const;       // Returns current number of elements in the list
-        T get(int index) const;   // Returns element at the given index
+        ArrayList() {
+            arr = nullptr;
+            size = 0;
+            capacity = 5; 
+            arr = new T[capacity];  // Allocate initial memory
+        }
+        
+        ~ArrayList() {
+            delete[] arr;
+        }
+        
+        void addToEnd(T value) {
+            // Checking if need to be resize
+            if (size >= capacity) {
+                resize();
+            }
+            
+            // Add new element at the end
+            arr[size] = value;
+            size++;
+        }
+        
+        void addToStart(T value) {
+            if (size >= capacity) {
+                resize();
+            }
+            
+            // Shifting all elements one position to the right
+            for (int i = size; i > 0; i--) {
+                arr[i] = arr[i-1];
+            }
+            
+            // Add new element at the beginning
+            arr[0] = value;
+            size++;
+        }
+        
+        void addAtRandom(T value) {
+            // If list is empty -> add to the end
+            if (size == 0) {
+                addToEnd(value);
+                return;
+            }
+            
+            // Set up random number generator
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> distrib(0, size);
+            int pos = distrib(gen);  // Generate random position between 0 and size (inclusive)
+            
+            // Handle special cases: adding at end or beginning
+            if (pos == size) {
+                addToEnd(value);
+            } else if (pos == 0) {
+                addToStart(value);
+            } else {
+                if (size >= capacity) {
+                    resize();
+                }
+                // Shifting elements to make space for new element
+                for (int i = size; i > pos; i--) {
+                    arr[i] = arr[i-1];
+                }
+                // Inserting new element at random position
+                arr[pos] = value;
+                size++;
+            }
+        }
+        
+        void removeLast() {
+            // Check if the array is not empty
+            if (size > 0) {
+                size--;  
+            }
+        }
+        
+        void removeFirst() {
+            // Checking if array is not empty
+            if (size > 0) {
+                // Shifting all elements one position to the left to overwrite the first element
+                for (int i = 0; i < size - 1; i++) {
+                    arr[i] = arr[i+1];
+                }
+                size--;  
+            }
+        }
+        
+        void removeRandom() {
+            // If array is empty -> nothing to remove
+            if (size == 0) {
+                return;
+            }
+            
+            // Set up random number generator
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> distrib(0, size - 1);
+            int pos = distrib(gen);  // Generate random position between 0 and size-1
+            
+            // Handle special cases: removing from end or beginning
+            if (pos == size - 1) {
+                removeLast();
+            } else if (pos == 0) {
+                removeFirst();
+            } else {
+                // Shifting elements to fill the gap left by the removed element
+                for (int i = pos; i < size - 1; i++) {
+                    arr[i] = arr[i+1];
+                }
+                size--;  
+            }
+        }
+        
+        bool contains(T value) {
+            // Linear search through the array
+            for (int i = 0; i < size; i++) {
+                if (arr[i] == value) {
+                    return true;  // Value found
+                }
+            }
+            return false;  // Value not found
+        }
 };
 
-// Include the implementation file to make template definitions available
-#include "ArrayList.cpp"
 
 #endif // ARRAYLIST_H
